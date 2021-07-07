dad491884b81d632b66461d7cbbcd728b1ccd546
DP23-ricanhead-co <63218215+DP23-riahead-co@users.noreply.github.com>
2021-07-07 09:16:52 +0700

    fourth change

--- Main/main.cpp
+++ Main/main.cpp
@@ -8,5 +8,6 @@ int main(){
     cout<<"Hello world"<<endl;
     cout<<"Goodbye world"<<endl;
     cout<<"on the Mini Duy branch"<<endl;
+    cout<<"change on the mini Duy branch"<<endl;
     return 0;
 }
