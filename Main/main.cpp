54da9d1e4eadcca7743d20bd3fdf25e6b8783f15
DP23-ricanhead-co <63218215+DP23-riahead-co@users.noreply.github.com>
2021-07-07 09:42:51 +0700

    sua loi nhanh

--- Main/main.cpp
+++ Main/main.cpp
@@ -8,6 +8,7 @@ int main(){
     cout<<"Hello world"<<endl;
     cout<<"Goodbye world"<<endl;
     cout<<"on the Mini Duy branch"<<endl;
-  cout<<"On branch master"<<endl;
+    cout<<"On branch master"<<endl;
+    cout<<"On branch Mini Duy master"<<endl;
     return 0;
 }
