9cc6b73137e16da4f705eb26a61640aa231f5cbf
DP23-ricanhead-co <63218215+DP23-riahead-co@users.noreply.github.com>
2021-07-07 09:53:25 +0700

    add hashfunc

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
-
+int hashFunc(string s, int sizeTab) {
+    long sum = 0, mul = 1;
+    for (int i = 0; i < s.length(); i++) {
+        mul = (i % 4 == 0) ? 1 : mul * 256;
+        sum += s.at(i) * mul;
+    }
+    return (int)(abs(sum) % sizeTab);
+}
 int main(){
-
-
-    cout<<"Hello world"<<endl;
-    cout<<"Goodbye world"<<endl;
-    cout<<"on the Mini Duy branch"<<endl;
-    cout<<"On branch master"<<endl;
-    cout<<"On branch Mini Duy master"<<endl;
+    string name;
+    cout<<"Nhap ten ban o day : ";
+    getline(cin,name);
+    cout<<hashFunc(name,101)<<endl;
     return 0;
 }
